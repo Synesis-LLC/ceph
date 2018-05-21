af297475f2958fb00f54c5fe9c81522922062d80
Yury Zhyhalka <yury.zhyhalka@synesis.ru>
2018-05-21 17:05:33 +0300

    restore log access cb

--- src/rgw/rgw_civetweb_frontend.cc
+++ src/rgw/rgw_civetweb_frontend.cc
@@ -191,7 +191,7 @@ int RGWCivetWebFrontend::run()
   cb.begin_request = civetweb_callback;
   cb.log_message = rgw_civetweb_log_callback;
   cb.log_access = rgw_civetweb_log_access_callback;
-  //cb.log_err_access = rgw_civetweb_log_err_access_callback;
+  cb.log_err_access = rgw_civetweb_log_err_access_callback;
   ctx = mg_start(&cb, this, options.data());
 
   /* Run metrics thread */
