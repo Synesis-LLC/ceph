371966652acd4dba958a5010673fa046fed09893
Aleksei Gutikov <aleksey.gutikov@synesis.ru>
2018-07-25 18:57:54 +0300

    doc: fix peering state model dot graph size

--- doc/scripts/gen_state_diagram.py
+++ doc/scripts/gen_state_diagram.py
@@ -160,8 +160,8 @@ class StateMachineRenderer(object):
                 top_level.append(state)
         print('Top Level States: ', top_level, file=sys.stderr)
         print('digraph G {')
-        print('\tsize="7,7"')
         print('\tcompound=true;')
+        print('\tnewrank=true;')
         for i in self.emit_state(top_level[0]):
             print('\t' + i)
         for i in self.edges.keys():
